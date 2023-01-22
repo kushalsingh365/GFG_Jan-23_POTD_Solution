a2fc2d92c084c1ddfb1f73f26b0b10d4244d21ad
Kushal Singh <81277166+kushalsingh365@users.noreply.github.com>
2023-01-22 14:23:35 +0530

    22_Jan_23

---
+++ 22) GCD Array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    int solve(int N, int K, vector<int> &arr) {
+        // code here
+        for(int i=1;i<N;i++) arr[i]+=arr[i-1];
+
+        vector<int> Vec;
+
+        for(int i=1;i*i<=arr[N-1];i++) {
+
+            if(arr[N-1]%i==0) {
+
+                Vec.push_back(i);
+
+                Vec.push_back(arr[N-1]/i);
+
+            }
+
+        }
+
+        
+
+        int Res=1;
+
+        for(int fact : Vec) {
+
+            int count=0;
+
+            for(int a : arr) {
+
+                if(a%fact==0) {
+
+                    count++;
+
+                }
+
+            }
+
+            if(count>=K) Res=max(Res,fact);
+
+        }
+
+        return Res;
+
+
+    }
+};
